58f2ee5286c29ade02c339d4157748eba341df0b
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-18 10:25:03 +0200

    fix cards images

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -16,7 +16,7 @@ export default {
 <template>
     <div class="col">
         <div class="card rounded-0 my-2">
-            <img :src="card.card_images" class="card-img-top" :alt="card.name">
+            <img :src="card.card_images[0].image_url" class="card-img-top" :alt="card.name">
             <div class="card-body">
                 <h5 class="card-title">{{ card.name }}</h5>
                 <p class="card-text">{{ card.type }}</p>
